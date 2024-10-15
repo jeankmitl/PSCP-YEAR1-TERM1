4834771788c69e4a04d386792aabeece1f6d2dad
jeankmitl <67070279@kmitl.ac.th>
2024-10-15 18:49:38 +0700

    Added Seeker.py

--- week9/seeker.py
+++ week9/seeker.py
@@ -3,5 +3,20 @@ def main():
     '''seeker'''
     word = input()
 
-    for i in range(len(word)):
-        if word[i].isnumeric:
+    # adds numbers to a pool, stops if the next letter is alphabhet
+    # it makes a number, adds to a total
+    numbers = 0
+    temp = ''
+    for i in word:
+        if i.isnumeric():
+            temp += i
+        else:
+            if temp:
+                numbers += (int(temp))
+                temp = ''
+
+    #remainders
+    if temp:
+        numbers += int(temp)
+    print(numbers)
+main()
